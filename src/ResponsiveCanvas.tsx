996cbf36c72171f5352ecfadb12a02c13ebffc20
Kotaro7750 <7750koutarou@gmail.com>
2025-05-10 13:43:50 +0900

    Change: Remove bootstrap

--- src/ResponsiveCanvas.tsx
+++ src/ResponsiveCanvas.tsx
@@ -38,7 +38,7 @@ export function ResponsiveCanvas(props: { sensitivity: any[], draw: (ctx: Canvas
 
   return (
     <Box width={'100%'} height={'100%'} ref={containerRef}>
-      <canvas className='d-block w-100 h-100' ref={canvasRef} />
+      <canvas style={{ display: 'block', width: '100%', height: '100%' }} ref={canvasRef} />
     </Box>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import './scss/bootstrap-custom.scss';
-import 'bootstrap-icons/font/bootstrap-icons.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
