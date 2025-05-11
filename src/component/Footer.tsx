daec2fd59eb6345b632f0c0a0a86ea7f15a93c8d
Kotaro7750 <7750koutarou@gmail.com>
2025-05-11 17:57:53 +0900

    Fix: Unaligned version string in footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,5 +1,5 @@
 import _ from 'react';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 
 interface FooterProps {
   height: number;
@@ -7,14 +7,14 @@ interface FooterProps {
 
 export function Footer({ height }: FooterProps) {
   return (
-    <AppBar position='fixed' color='primary' sx={{ bottom: 0, top: 'auto' }}>
-      <Container>
-        <Toolbar variant='dense' sx={{ minHeight: `${height}px`, height: `${height}px`, justifyContent: 'end' }}>
+    <AppBar position='fixed' color='primary' sx={{ bottom: 0, top: 'auto', width: '100%' }}>
+      <Box sx={{ width: '100%', margin: 0 }}>
+        <Toolbar variant='dense' sx={{ minHeight: `${height}px`, height: `${height}px`, justifyContent: 'end', width: '100%' }}>
           <Typography>
             Version {__APP_VERSION__}
           </Typography>
         </Toolbar>
-      </Container>
+      </Box>
     </ AppBar>
   );
 }
