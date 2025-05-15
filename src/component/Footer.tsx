31e9d2d7c042f9b916a589f19d3f92a479cd96fb
Kotaro7750 <7750koutarou@gmail.com>
2025-05-15 14:53:22 +0900

    Add: Add github repository link to footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,5 +1,6 @@
 import _ from 'react';
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Grid, IconButton, Toolbar, Typography } from '@mui/material';
+import { GitHub } from '@mui/icons-material';
 
 interface FooterProps {
   height: number;
@@ -10,9 +11,18 @@ export function Footer({ height }: FooterProps) {
     <AppBar position='fixed' color='primary' sx={{ bottom: 0, top: 'auto', width: '100%' }}>
       <Box sx={{ width: '100%', margin: 0 }}>
         <Toolbar variant='dense' sx={{ minHeight: `${height}px`, height: `${height}px`, justifyContent: 'end', width: '100%' }}>
-          <Typography>
-            Version {__APP_VERSION__}
-          </Typography>
+          <Grid container justifyContent='space-between' alignItems='center' spacing={2}>
+            <Grid container alignItems={'center'} >
+              <IconButton href='https://github.com/Kotaro7750/typer-concierge-web' target='_blank'>
+                <GitHub fontSize='medium' sx={{ color: 'white' }} />
+              </IconButton>
+            </Grid>
+            <Grid>
+              <Typography>
+                Version {__APP_VERSION__}
+              </Typography>
+            </Grid>
+          </Grid>
         </Toolbar>
       </Box>
     </ AppBar>
